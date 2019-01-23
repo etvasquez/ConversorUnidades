d03ce68a536195bdd4a670f0027f62820ca4bb58
etvasquez <eriivasquez.ev.ev@gmail.com>
2019-01-23 11:44:29 -0500

    Add files via upload

---
+++ Conversor Unidades/js/unidades.js
@@ -0,0 +1,147 @@
+var unidades = {
+    'Eliga la equivalencia ...': {
+        'grupo': ' ',
+        'unidades': [
+        ],
+        'equivalencias': {
+            
+        }
+    },
+    'Longitud': {
+        'grupo': 'Longitud',
+        'unidades': [
+            'kilometro',
+            'metro',
+            'centimetro',
+            'milimetro',
+            'micrometro',
+            'nanometro',
+            'milla',
+            'yarda',
+            'pie',
+            'pulgada'
+        ],
+        'equivalencias': {
+            'kilometro': {
+                'kilometro': 1,
+                'metro': 1000,
+                'centimetro': 100000,
+                'milimetro': 1e+6,
+                'micrometro': 1e+9,
+                'nanometro': 1e+12,
+                'milla': 0.621371,
+                'yarda': 1093.61,
+                'pie': 3280.84,
+                'pulgada': 39370.1
+            },
+            'metro': {
+                'kilometro': 0.001,
+                'metro': 1,
+                'centimetro': 100,
+                'milimetro': 1000,
+                'micrometro': 1e+6,
+                'nanometro': 1e+9,
+                'milla': 0.000621371,
+                'yarda': 1.09361,
+                'pie': 3.28084,
+                'pulgada': 39.3701
+            },
+            'centimetro': {
+                'kilometro': 1e-5,
+                'metro': 0.01,
+                'centimetro': 1,
+                'milimetro': 10,
+                'micrometro': 10000,
+                'nanometro': 1e+7,
+                'milla': 6.2137e-6,
+                'yarda': 0.0109361,
+                'pie': 0.0328084,
+                'pulgada': 0.393701
+            },
+            'milimetro': {
+                'kilometro': 1e-6,
+                'metro': 0.001,
+                'centimetro': 0.1,
+                'milimetro': 1,
+                'micrometro': 1000,
+                'nanometro': 1e+6,
+                'milla': 6.2137e-7,
+                'yarda': 0.00109361,
+                'pie': 0.00328084,
+                'pulgada': 0.0393701
+            },
+            'micrometro': {
+                'kilometro': 1e-9,
+                'metro': 1e-6,
+                'centimetro': 1e-4,
+                'milimetro': 0.001,
+                'micrometro': 1,
+                'nanometro': 1000,
+                'milla': 6.2137e-10,
+                'yarda': 1.0936e-6,
+                'pie': 3.2808e-6,
+                'pulgada': 3.937e-5
+            },
+            'nanometro': {
+                'kilometro': 1e-12,
+                'metro': 1e-9,
+                'centimetro': 1e-7,
+                'milimetro': 1e-6,
+                'micrometro': 0.001,
+                'nanometro': 1,
+                'milla': 6.2137e-13,
+                'yarda': 1.0936e-9,
+                'pie': 3.2808e-9,
+                'pulgada': 3.937e-8
+            },
+            'milla': {
+                'kilometro': 1.60934,
+                'metro': 1609.34,
+                'centimetro': 160934,
+                'milimetro': 1.609e+6,
+                'micrometro': 1.609e+9,
+                'nanometro': 1.609e+12,
+                'milla': 1,
+                'yarda': 1760,
+                'pie': 5280,
+                'pulgada': 63360
+            },
+            'yarda': {
+                'kilometro': 0.0009144,
+                'metro': 0.9144,
+                'centimetro': 91.44,
+                'milimetro': 914.4,
+                'micrometro': 914400,
+                'nanometro': 9.144e+8,
+                'milla': 0.000568182,
+                'yarda': 1,
+                'pie': 3,
+                'pulgada': 36
+            },
+            'pie': {
+                'kilometro': 0.0003048,
+                'metro': 0.3048,
+                'centimetro': 30.48,
+                'milimetro': 304.8,
+                'micrometro': 304800,
+                'nanometro': 3.048e+8,
+                'milla': 0.000189394,
+                'yarda': 0.333333,
+                'pie': 1,
+                'pulgada': 12
+            },
+            'pulgada': {
+                'kilometro': 2.54e-5,
+                'metro': 0.0254,
+                'centimetro': 2.54,
+                'milimetro': 25.4,
+                'micrometro': 25400,
+                'nanometro': 2.54e+7,
+                'milla': 1.5783e-5,
+                'yarda': 0.0277778,
+                'pie': 0.0833333,
+                'pulgada': 1
+            }
+        }
+    },
+};
